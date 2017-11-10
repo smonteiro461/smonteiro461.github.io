63099f31930df4c1ba27e647bba0f46624c57862
smonteiro461 <smonteiro461@github.com>
2017-11-10 11:13:59 +0000

    delete

--- Grupo/Untitled-1.js
+++
@@ -1,9 +0,0 @@
-var img;
-function preload() {
-  img = loadImage("Eva.png");
-}
-function setup() {
-  // Top-left corner of the img is at (0, 0)
-  // Width and height are the img's original width and height
-  image(img, 0, 0);
-}
