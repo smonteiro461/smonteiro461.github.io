30c2759f9be8ee1ec680744bf4ba83d7ea29ec9e
smonteiro461 <smonteiro461@github.com>
2017-11-13 14:31:33 +0000

    3

--- sketch.js
+++ sketch.js
@@ -1,41 +1,51 @@
-var yoff = 0.0;  
+// Ruben Matos 3150547  
+// Daniel Costa 3150548
+// Sofia Monteiro 3150424
+// Jessica Machado 3150550
+// Eva magia
+// 2017/2018 
+// 1º Semestre 
+// Laboratório de Projecto 1 
+// Design Gráfico e Multimédia 
+// Escola Superior de Artes e Design das Caldas da Rainha 
+// Professor - Marco Heleno 
+
+
+var yoff = 0.0;        // 2nd dimension of perlin noise
 var img;
 
-function setup()
-{
 
-  createCanvas(596, 842);
+function setup() 
+{
+  createCanvas(796, 1042, SVG);
   frameRate(1);
-  img = loadImage("eva1.png");
- 
+  img = loadImage("lateral2.png");  // Carregar a imagem 
 }
 
-function draw() 
-{
+function draw() {
+
   background(255);
-  stroke(60);
-  //criar retangulo
-  rect (width/2, height/2, 500, 750, 20);
-  rectMode (CENTER);
-  stroke(30);
   noFill();
- 
-  //criar linhas random 
+  stroke(0);
+  //rect (width/2, height/2, 500, 750, 20);
+  //rectMode (CENTER);
+
+  //criar linhas em movimento
   for (var y=0; y<height+10; y+=10) 
   {
     beginShape();
     var xoff = 0;
     for (var x=0; x<width+10; x+=10) 
-    { //criar noise
+    { // Criar noise 
       var a = noise(xoff, yoff)+1;
-      //definir vertex
+      // defenir vertex
       vertex(x, y*a); 
+      //  defenir X 
       xoff += 0.05;
-    }
+    } // defenir Y 
     yoff += 0.03;
     endShape();
   }
-}  
   // localização da imagem
   image(img, 0, 0);
   lateral2.resize(500, 750);
@@ -57,3 +67,4 @@ function draw()
       save("meuProjecto.svg");
     }
   }
+}
